f6596000e904c6b7486021ab3ba48fd25adff8b4
cbdweb <nik@cbdweb.net>
2021-02-16 15:57:31 +1100

    remove FC_captcha_fields shortcode to signatures form;

--- fs-signatures.php
+++ fs-signatures.php
@@ -1121,7 +1121,6 @@ function fs_page_sign ( $atts ) {
 
             <tr><td class="leftcol"><input id="simpleTuring<?=($popup ? "_popup" : "");?>" name="areYouThere" type="checkbox" value="y" class="inputc"></td><td class="medfont">Tick this box to show you are not a robot</td></tr>
             <tr><td class="leftcol">Comment:</td><td class="rightcol"><textarea name="excerpt" class="inputc"></textarea><br><div class="smallfont">Comments are subject to moderation</div></td></tr>
-            <tr><td colspan="2"><?=do_shortcode( '[ FC_captcha_fields width=”200″ height=”30″ ]');?></td> </tr>
             <tr><td colspan="2"><button type="button" id="saveButton<?=($popup ? "_popup" : "");?>">Save</button></td></tr>
             <tr><td colspan="2"><div id="ajax-loading<?=($popup ? "_popup" : "");?>" class="farleft"><img src="<?php echo get_site_url();?>/wp-includes/js/thickbox/loadingAnimation.gif"></div></td></tr>
             <tr><td colspan="2"><div id="returnMessage<?=($popup ? "_popup" : "");?>"></div></td></tr>
